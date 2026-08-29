6782163f9007449391c385e6c9d096599373a422
bbmseries-maxn <bbmseries@gmail.com>
2026-08-29 16:29:50 +0300

    seper admin actions

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { marketDb } from './app/core/db/market-db';
+
+(window as any).marketDb = marketDb;
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
